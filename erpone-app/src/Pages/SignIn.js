5a911835ecf70f776e1b156c124cdcc3889b0be2
mbartoszewski <mbartoszewskii@gmail.com>
2021-03-30 23:17:22 +0200

    test

--- erpone-app/src/Pages/SignIn.js
+++ erpone-app/src/Pages/SignIn.js
@@ -47,7 +47,7 @@ const useStyles = makeStyles((theme) => ({
 		backgroundColor: theme.palette.secondary.main,
 	},
 	form: {
-		width: '100%', // Fix IE 11 issue.
+		width: '100%', // Fix IE 11 issue...test
 		marginTop: theme.spacing(1),
 	},
 	submit: {
